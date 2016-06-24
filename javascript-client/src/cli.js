c51205b366afe22df3b61d36230ad0d0c09e9d87
Cedric Johnson <cjohns0913@gmail.com>
2016-06-24 16:57:08 -0400

    dealt with delimiter...after two hours :oncoming_bus:

--- javascript-client/src/cli.js
+++ javascript-client/src/cli.js
@@ -9,23 +9,33 @@ cli
 
 // connect mode
 let server
+let dt = new Date().toLocaleString()
 
 cli
-  .mode('connect <port> [host]')
-  .delimiter('connected:')
+// <<<<<<< Updated upstream
+//   .mode('connect <port> [host]')
+// =======
+  .mode('connect <username> <host> <port>')
+  .delimiter(' ')
+// >>>>>>> Stashed changes
   .init(function (args, callback) {
+    cli._baseExitMode = cli._exitMode
+    cli._exitMode = function (args) {
+      return cli._baseExitMode(args)
+    }
     server = net.createConnection(args, () => {
+      server.write(args.username + '\n')
       const address = server.address()
-      this.log(`connected to server ${address.address}:${address.port}`)
+      this.log(`${args.username} connected to server ${address.address}:${address.port}` + ' ' + dt)
+      this.delimiter(`${args.username}`)
       callback()
     })
-
-    server.on('data', (data) => {
+    server.on('data', (data, dt) => {
       this.log(data.toString())
     })
 
     server.on('end', () => {
-      this.log('disconnected from server :(')
+      this.log('${args.username} disconnected from server :(')
     })
   })
   .action(function (command, callback) {
